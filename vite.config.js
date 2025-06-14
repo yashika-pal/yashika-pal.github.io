27446ebebf5f5a23f3bddbe6986713d23bdeda70
Yashika Pal <yashibang2004@gmail.com>
2025-06-14 23:20:33 +0530

    added changes

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,11 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/', 
   plugins: [react()],
-  css:{
+  css: {
     modules: {
       localsConvention: 'camelCase',
-    }
-  }
+    },
+  },
 })
